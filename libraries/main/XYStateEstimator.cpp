3892e8dda933db2f520fcf5c15efc9d4f9864532
yukiwatanabe05 <ywatanabe@hmc.edu>
2025-03-27 15:44:43 -0700

    4.4 Adding yaw calculations

--- libraries/main/XYStateEstimator.cpp
+++ libraries/main/XYStateEstimator.cpp
@@ -32,9 +32,12 @@ void XYStateEstimator::updateState(imu_state_t * imu_state_p, gps_state_t * gps_
     ///////////////////////////////////////////////////////////////////
     // INSERT YAW, X and Y CALCULATION HERE
     //////////////////////////////////////////////////////////////////
-    state.x = RADIUS_OF_EARTH_M * (degToRad(gps_state_p->lon) - degToRad(origin_lon)) * cos(degToRad(gps_state_p->lat))
+    state.x = RADIUS_OF_EARTH_M * (degToRad(gps_state_p->lon) - degToRad(origin_lon)) * cos(degToRad(origin_lat))
     state.y = RADIUS_OF_EARTH_M * (degToRad(gps_state_p->lat) - degToRad(origin_lat))
-    state.yaw =      // to do
+    
+    float heading_rad = imu_state_p->heading*PI/180.0;
+    float yaw_rad = -heading_rad + PI/2.0;
+    state.yaw = angleDiff(yaw_rad); // to do
   }
   else{
     gpsAcquired = 0;
